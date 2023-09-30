28549c6a0294b2f5dd6186d796234326d906123d
Uche Godfrey <uchegodfrey@Uches-MacBook-Pro-max.local>
2023-09-29 20:09:52 -0400

    first commit

---
+++ MovieDB/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  MovieDB
+//
+//  Created by Uche Godfrey on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
