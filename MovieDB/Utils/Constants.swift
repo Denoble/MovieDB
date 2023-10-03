2f1bf5a07d8ad6bae1ce08e0b1c76387517cf0ad
Lawrence <consultant@MBP-C02ZT45VMD6M-TESCHROEDER.local>
2023-10-03 14:36:37 -0400

    Create ViewModels & WebService

---
+++ MovieDB/Utils/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  MovieDB
+//
+//  Created by Lawrence on 10/2/23.
+//
+
+import Foundation
+
+struct Constants {
+    static let baseSearchUrl = "https://api.themoviedb.org/3/search/movie"
+    static let basePosterUrl = "https://image.tmdb.org/t/p/original"
+    static let headers = ["accept": "application/json",
+                          "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDliNzUzNmYwNTRhNGRhZTY5NTBkOGIwOWQ3OWNhOSIsInN1YiI6IjY1MTg4Mjk3OTY3Y2M3MzQyOGNhZTlhNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BKKHnUe07Da9yYC4CorzM1TSydbobPuD4bsgZzGx-_0"]
+    static let adult = URLQueryItem(name: "include_adult", value: "false")
+    static let language = URLQueryItem(name: "language", value: "en-US")
+}
---
+++ MovieDB/ViewModel/MovieDetailViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  MovieDetailViewModel.swift
+//  MovieDB
+//
+//  Created by Lawrence on 10/2/23.
+//
+
+import Foundation
+import SwiftUI
+
+class MovieDetailViewModel: ObservableObject {
+    let imageCache: ImageCacheHelper
+    let webService: APIImplement
+    init(webService: APIImplement, imageCashe: ImageCacheHelper) {
+        self.webService = webService
+        self.imageCache = imageCashe
+    }
+    
+    func getPoster(posterPath: String) async throws -> UIImage? {
+        if let cachedImage = imageCache.getImage(path: posterPath) {
+            return cachedImage
+        } else {
+            let networkRequest = NetworkRequest(baseUrl: Constants.basePosterUrl, path: posterPath, params: [], type: .GET, headers: [:])
+            do {
+                let data = try await webService.fetchImageData(request: networkRequest)
+                if let data = data {
+                    guard let uiImage = UIImage(data: data) else { return nil }
+                    imageCache.setImage(image: uiImage, path: posterPath)
+                    return uiImage
+                }
+            } catch {
+                throw error
+            }
+            return nil
+        }
+    }
+}
